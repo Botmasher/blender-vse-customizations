c325e7f811e67b8b1b1ffecfa4dab6c2a68a59a9
Botmasher <languagejosh@hotmail.com>
2016-04-05 17:01:58 -0400

    Define clear keyframes function and UI; add rotation transitions to class

--- vse_transitions.py
+++ vse_transitions.py
@@ -3,7 +3,12 @@ import math
 
 class Transition (object):
     def handler ():
-        effect = {'left':Transition.left,'right':Transition.right,'top':Transition.top,'bottom':Transition.bottom,'fade':Transition.fade,'scale_up':Transition.scale_up,'scale_down':Transition.scale_down}
+        # all transitions to call mapped to transition_type string
+        effect = {'left':Transition.left, 'right':Transition.right,
+                'top':Transition.top, 'bottom':Transition.bottom,
+                'fade':Transition.opacity_down, 'unfade':Transition.opacity_up, 
+                'scale_up':Transition.scale_up, 'scale_down':Transition.scale_down, 
+                'clockwise':Transition.rotate_clock,'counterclock':Transition.rotate_counterclock}
         # references to active transform strip and its parent
         strip = bpy.context.scene.sequence_editor.active_strip
         parent = strip.input_1
@@ -37,13 +42,13 @@ class Transition (object):
 
     def set (strip, property_name, end_value, starting_frame, duration):
         """Move to starting frame, set a keyframe for a property, move to final frame (starting frame plus duration), change property value, set keyframe for the property."""
-        # move to and keyframe the starting frame
+        # move to and keyframe the starting frame (just leaving its current value)
         bpy.context.scene.frame_current = starting_frame
         strip.keyframe_insert (property_name, -1, starting_frame)
         # move to and keyframe the ending frame with the final property value
         bpy.context.scene.frame_current += duration
         
-        # set keyframe values on the appropriate property_name
+        # give the relevant property_name a new ending value
         if property_name == 'translate_start_x':
             strip.translate_start_x = end_value
         elif property_name == 'translate_start_y':
@@ -52,6 +57,8 @@ class Transition (object):
             strip.scale_start_x = end_value
         elif property_name == 'scale_start_y':
             strip.scale_start_y = end_value
+        elif property_name == 'rotation_start':
+            strip.rotation_start = end_value
         elif property_name == 'blend_alpha':
             strip.blend_alpha = end_value
         else:
@@ -60,55 +67,99 @@ class Transition (object):
         strip.keyframe_insert (property_name, -1, starting_frame+duration)
         return None
 
+    def clear (strip):
+        """Remove all keyframes from the transition properties fields and reset their values"""
+        # move to the first frame in this strip
+        bpy.context.scene.frame_current = strip.frame_start
+        # get list of all properties that Transition messes with
+        properties = Transition.strip_properties(strip)
+        # count through every frame looking for keyframes
+        for frame in range (strip.frame_start-1, strip.frame_start+strip.frame_final_duration+1):
+            # attempt to remove keyframes on all transition properties this frame
+            for prop in properties:
+                try:
+                    strip.keyframe_delete(prop, -1, frame)
+                except:
+                    pass
+            # try to skip ahead to the next keyframe
+            try:
+                bpy.ops.screen.keyframe_jump()
+            except:
+                pass
+            # set the frame variable to match skip ahead
+            frame = bpy.context.scene.frame_current
+        # reset transform properties to default values
+        strip.translate_start_x = 0.0
+        strip.translate_start_y = 0.0
+        strip.blend_alpha = 1.0
+        strip.use_uniform_scale = True
+        strip.scale_start_x = 1.0
+        strip.rotation_start = 1.0
+
     def strip_properties (strip):
         """Strip property values to transition when given a property name string as key"""
-        return {'translate_start_x': strip.translate_start_x,
-                'translate_start_y': strip.translate_start_y,
-                'scale_start_x': strip.scale_start_x,
-                'scale_start_y': strip.scale_start_y,
-                'blend_alpha': strip.blend_alpha
+        return {'blend_alpha': strip.blend_alpha, 
+                'translate_start_x': strip.translate_start_x, 
+                'translate_start_y': strip.translate_start_y, 
+                'scale_start_x': strip.scale_start_x, 
+                'scale_start_y': strip.scale_start_y, 
+                'rotation_start': strip.rotation_start
                 }
 
     def left (strip, start_frame, duration):
         """Set up values to slide a strip's position to or from left edge of screen"""
-        end_value = -(Transition.get_screen_dimensions(strip)[0])
+        end_value = -(Transition.get_screen_dimensions(strip)[0]) * strip.transition_strength
         # setup the keyframing function
         Transition.set (strip, 'translate_start_x', end_value, start_frame, duration)
 
     def right (strip, start_frame, duration):
         """Set up values to slide a strip's position to or from right edge of screen"""
-        end_value = Transition.get_screen_dimensions(strip)[0]
+        end_value = Transition.get_screen_dimensions(strip)[0] * strip.transition_strength
         # setup the keyframing function
         Transition.set (strip, 'translate_start_x', end_value, start_frame, duration)
 
     def top (strip, start_frame, duration):
         """Set up values to slide a strip's position to or from top edge of screen"""
-        end_value = Transition.get_screen_dimensions(strip)[1]
+        end_value = Transition.get_screen_dimensions(strip)[1] * strip.transition_strength
         # setup the keyframing function
         Transition.set (strip, 'translate_start_y', end_value, start_frame, duration)
 
     def bottom (strip, start_frame, duration):
         """Set up values to slide a strip's position to or from bottom edge of screen"""
-        end_value = -(Transition.get_screen_dimensions(strip)[1])
+        end_value = -(Transition.get_screen_dimensions(strip)[1]) * strip.transition_strength
         # setup the keyframing function
         Transition.set (strip, 'translate_start_y', end_value, start_frame, duration)
 
-    def fade (strip, start_frame, duration):
+    def opacity_up (strip, start_frame, duration):
+        """Set up values to slide a strip's opacity to or from opaque"""
+        end_value = 0.0 + strip.transition_strength
+        # setup the keyframing function
+        Transition.set (strip, 'blend_alpha', end_value, start_frame, duration)
+
+    def opacity_down (strip, start_frame, duration):
         """Set up values to slide a strip's opacity to or from transparent"""
-        end_value = 0.0
+        end_value = 1.0 - strip.transition_strength
         # setup the keyframing function
         Transition.set (strip, 'blend_alpha', end_value, start_frame, duration)
 
     def scale_up (strip, start_frame, duration):
-        end_value = strip.scale_start_x * 3.5
+        end_value = strip.scale_start_x * 3.5 * strip.transition_strength
         Transition.set (strip, 'scale_start_x', end_value, start_frame, duration)
         if not strip.use_uniform_scale:
-            end_value = strip.scale_start_y *3.5
+            end_value = strip.scale_start_y *3.5 * strip.transition_strength
             Transition.set (strip, 'scale_start_y', end_value, start_frame, duration)
 
     def scale_down (strip, start_frame, duration):
-        end_value = strip.scale_start_x * 0.05
+        end_value = strip.scale_start_x * 0.05 / strip.transition_strength
         Transition.set (strip, 'scale_start_x', end_value, start_frame, duration)
         if not strip.use_uniform_scale:
-            end_value = strip.scale_start_y * 0.05
+            end_value = strip.scale_start_y * 0.05 / strip.transition_strength
             Transition.set (strip, 'scale_start_y', end_value, start_frame, duration)
+
+    def rotate_clock (strip, start_frame, duration):
+        end_value = strip.rotation_start + (360 * strip.transition_strength)
+        Transition.set (strip, 'rotation_start', end_value, start_frame, duration)
+
+    def rotate_counterclock (strip, start_frame, duration):
+        end_value = strip.rotation_start - (360 * strip.transition_strength)
+        Transition.set (strip, 'rotation_start', end_value, start_frame, duration)
--- vse_transitions_ui.py
+++ vse_transitions_ui.py
@@ -6,14 +6,17 @@ import vse_transitions
 # Custom properties for Transition object to read as transition options
 #
 bpy.types.TransformSequence.transition_type = EnumProperty(
-    items = [('scale_down', 'Scale down', 'to or from small scale (zoom out)'),
+    items = [('counterclock', 'Counterclock', 'to or from counterclockwise rotation'),
+             ('clockwise', 'Clockwise', 'to or from a clockwise rotation'),
+             ('scale_down', 'Scale down', 'to or from small scale (zoom out)'),
              ('scale_up', 'Scale up', 'to or from large scale (zoom in)'),
+             ('unfade', 'Unfade', 'to or from opaque'),
              ('fade', 'Fade', 'to or from transparent'),
              ('bottom', 'Bottom', 'to or from bottom'),
              ('top', 'Top', 'to or from top'),
              ('right', 'Right', 'to or from right edge'),
              ('left', 'Left', 'to or from left edge')],
-    name = 'Transition Type',
+    name = 'Type',
     description = 'Type of transition to add to this strip'
     )
 
@@ -29,6 +32,14 @@ bpy.types.TransformSequence.transition_frames = IntProperty (
     name = 'Duration (frames)', 
     default = 10,
     description = 'Number of frames the transition will last'
+    )
+
+bpy.types.TransformSequence.transition_strength = FloatProperty (
+    name = 'Strength', 
+    default = 1.0, 
+    min = 0.0, 
+    max = 2.0, 
+    description = 'Change the impact of the effect (defaults to 1.0 = 100%)'
     )
 
 # bpy.types.TransformSequence.transition_in = BoolProperty(
@@ -58,6 +69,7 @@ class CustomTransitionsPanel (bpy.types.Panel):
         self.layout.row().prop(strip,'transition_type')
         # integer input for setting transition length in frames
         self.layout.row().prop(strip,'transition_frames')
+        self.layout.row().prop(strip,'transition_strength')
         # check boxes for toggling transition in/out/mid
         #row_1 = self.layout.split(0.4)
         #row_1.prop(strip,'transition_in')
@@ -68,6 +80,7 @@ class CustomTransitionsPanel (bpy.types.Panel):
         # execute button
         self.layout.separator()
         self.layout.operator('strip.transition_add', text="Keyframe Transition")
+        self.layout.operator('strip.transition_delete', text="/!\\ Clear Keyframe Fields /!\\")
 
 class AddTransition (bpy.types.Operator):
     bl_label = 'Add Transitions'
@@ -77,16 +90,29 @@ class AddTransition (bpy.types.Operator):
         try:
             Transition.handler()
         except:
-            raise NotImplementedError('Method %s not implemented in Transition object'%func)
+            raise NotImplementedError('Method Transition.handler() not implemented in Transition object')
+        return{'FINISHED'}
+
+class DeleteTransition (bpy.types.Operator):
+    bl_label = 'Delete Transitions'
+    bl_idname = 'strip.transition_delete'
+    bl_description = 'Delete ALL visible keyframes from strip transition fields (opacity, position, scale, rotation)'
+    def execute (self, context):
+        try:
+            Transition.clear(context.scene.sequence_editor.active_strip)
+        except:
+            raise NotImplementedError('Method Transition.clear(strip) not implemented in Transition object')
         return{'FINISHED'}
 
 def register():
     bpy.utils.register_class(CustomTransitionsPanel)
     bpy.utils.register_class(AddTransition)
+    bpy.utils.register_class(DeleteTransition)
 
 def unregister():
     bpy.utils.unregister_class(CustomTransitionsPanel)
     bpy.utils.unregister_class(AddTransition)
+    bpy.utils.unregister_class(DeleteTransition)
 
 if __name__ == '__main__':
     register()
