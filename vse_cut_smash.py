76c0b564e63082a4b03d72cb182ee97f3fa818c6
Botmasher <languagejosh@hotmail.com>
2016-04-07 18:59:38 -0400

    Work on fixes for only one strip cutting, neither closing gap

--- vse_cut_smash.py
+++ vse_cut_smash.py
@@ -15,7 +15,10 @@ class CutSmashPanel (bpy.types.Panel):
     bl_region_type = 'UI'
     def draw (self, context):
         strip = bpy.context.scene.sequence_editor.active_strip
-        self.layout.row().prop(strip,'cut_smash_direction', expand=True)
+        try:
+            self.layout.row().prop(strip,'cut_smash_direction', expand=True)
+        except:
+            # /!\ location: <unknown location>:-1
         # execute button
         self.layout.operator('strip.cut_smash', text="Jumpcut Smash")
 
@@ -23,17 +26,16 @@ class CutSmashOperator (bpy.types.Operator):
     bl_label = 'Jumpcut Smash'
     bl_idname = 'strip.cut_smash'
     bl_description = 'Cut strip at current location and place against previous strip'
-    bl_space_type = 'SEQUENCE_EDITOR'
-    bl_region_type = 'UI'
     def execute (self, context):
-        for strip in bpy.context.scene.sequence_editor.sequences_all:
+        for strip in context.scene.sequence_editor.sequences_all:
             if strip.select:
                 strip.frame_offset_start = bpy.context.scene.frame_current-strip.frame_start
+                print (strip)
                 # store frame location of the gap created by this soft cut
-                gap_frame = strip.frame_start + strip.frame_offset_start - 1
+                #gap = strip.frame_start + strip.frame_offset_start - 1
         # skip to the frame before this strip and close gap
-        bpy.context.scene.frame_current = gap_frame
-        bpy.ops.sequencer.gap_remove()
+        #context.scene.frame_current = gap
+        #bpy.ops.sequencer.gap_remove()
         return{'FINISHED'}
 
 def register():
