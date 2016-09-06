f8152c8a6ec6c8b41bb7f7b6952b4771f1edde25
Botmasher <languagejosh@hotmail.com>
2016-09-06 11:21:26 -0400

    Add shape key value keyframe and comments on steps to integrate audio

--- audio-drives-shapekey.py
+++ audio-drives-shapekey.py
@@ -9,6 +9,7 @@ import bpy
 # ?3. add your audio file
 
 obj = bpy.context.object
+starting_frame = 0
 
 # no keys, add a basis key first
 if obj.data.shape_keys == None:
@@ -17,7 +18,19 @@ audio_key = obj.shape_key_add()
 audio_key.name = "Audio driven key"
 audio_key.value = 1.0
 
+# add keyframe to shape key value
+bpy.context.scene.frame_current = 0
+audio_key.keyframe_insert("value",frame=starting_frame)
 
+# bake sound to the shape key fcurve
+
+# add modifier > envelope > add point
+
+# mess with r, min, max vals (can auto somehow?)
+
+# set video to length of the audi
+
+# add same audio file to vse starting at frame n
 
 # driving from this key --
 # bpy.context.object.data.shape_keys.key_blocks['Name'].driver_add("PATH TO THE PROPERTY TO DRIVE") # analogous to the fcurve's data path
