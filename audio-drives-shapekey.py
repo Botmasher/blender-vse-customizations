92eab1ca0bd170d1a9c973a1a980007bc9674bcb
Botmasher <languagejosh@hotmail.com>
2016-09-07 00:32:56 -0400

    Bake audio file with envelope modifier to shape key fcurve - Audio Shape Key

--- audio-drives-shapekey.py
+++ audio-drives-shapekey.py
@@ -8,6 +8,7 @@ import bpy
 # 2. click "Run script" with this script open in the text editor
 # ?3. add your audio file
 
+sound_file_path = './directory/filename.ogg'
 obj = bpy.context.object
 starting_frame = 0
 
@@ -15,20 +16,25 @@ starting_frame = 0
 if obj.data.shape_keys == None:
     obj.shape_key_add()
 audio_key = obj.shape_key_add()
-audio_key.name = "Audio driven key"
+audio_key.name = "audio-driven-key"
 audio_key.value = 1.0
 
 # add keyframe to shape key value
 bpy.context.scene.frame_current = 0
-audio_key.keyframe_insert("value",frame=starting_frame)
+kf = audio_key.keyframe_insert("value",frame=starting_frame)
 
 # bake sound to the shape key fcurve
+ctx = bpy.context.area.type
+bpy.context.area.type = 'GRAPH_EDITOR'
+bpy.ops.graph.sound_bake (sound_file_path)
 
 # add modifier > envelope > add point
+bpy.ops.graph.fmodifier_add(type='ENVELOPE')
+bpy.context.area.type = ctx
 
 # mess with r, min, max vals (can auto somehow?)
 
-# set video to length of the audi
+# set video to length of the audio
 
 # add same audio file to vse starting at frame n
 
