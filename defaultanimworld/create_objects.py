dab33e2c1a1c3ba327fb4c4147b89e61941e723b
Botmasher <languagejosh@hotmail.com>
2018-02-14 07:52:07 -1000

    Fill out add and delete obj from scene functions

--- defaultanimworld/create_objects.py
+++ defaultanimworld/create_objects.py
@@ -3,20 +3,36 @@ from mathutils import Vector
 
 ## Create new Blender objects within the current scene
 
+def create_objects(obj_settings={}):
+	"""Create and setup Blender objects in scene from an array of settings dicts"""
+	added_objects = []
+	for obj in obj_settings:
+		added_obj = bpy.data.objects.new(obj.name, None)
+		for attr in obj['attributes']:
+			setattr(obj, attr, obj['attributes'][attr])
+		bpy.context.scene.objects.link(added_obj)
+		added_objects.push(added_obj)
+	return added_objects
+
 objects_to_add = {
 	'data': {
 		'objects': [
 			{
 				'name': "Newly Created Object 0"
 				'primitive': 'cube',
-				'location': Vector(0.0, 0.0, 0.0)
+				'attributes': {
+					'location': Vector(0.0, 0.0, 0.0)
+				}
+			},
+			{
+				'name': "Newly Created Object 1"
+				'primitive': 'cube',
+				'attributes': {
+					'location': Vector(1.0, 1.0, 1.0)
+				}
 			}
 		]
 	}
 }
 
-# - set up recursive search like autoset_named_attributes
-# - add each object to scene
-added_object = bpy.data.objects.new(objects_to_add['data']['objects'][0].name, None) 
-added_object.location = objects_to_add['data']['objects'][0].location
-bpy.context.scene.objects.link(added_object)
+create_objects(objects_to_add['data']['objects'])
--- defaultanimworld/delete_objects.py
+++ defaultanimworld/delete_objects.py
@@ -1,7 +1,20 @@
 import bpy
 from mathutils import Vector
 
-## Delete existing Blender objects from the current scene
+def list_object_names(objects):
+	return map(lambda o: o.name, objects)
+
+def remove_objects(object_names, map_names=False):
+	"""Delete existing objects from scene where object name matches a name in the list"""
+	object_names = list_object_names(object_names) if map_names else object_names
+	bpy.ops.object.select_all(action="DESELECT")
+	for name in object_names:
+		try:
+			bpy.context.scene.objects[name].select = True
+		except:
+			pass 	# TODO handle exception: still selected but no longer exists
+		bpy.ops.object.delete()
+		#bpy.context.scene.objects[name].select = False
 
 objects_to_remove = {
 	'data': {
@@ -12,12 +25,3 @@ objects_to_remove = {
 		]
 	}
 }
-
-
-bpy.ops.object.select_all(action="DESELECT")
-try:
-	bpy.context.scene.objects[objects_to_remove['data']['objects'][0]['name']].select = True
-except:
-	#raise Exception("object deleted but no longer exists in bpy.data.objects")
-	pass 	# TODO handle exception: still selected but no longer exists
-bpy.ops.object.delete()
