2d1ca6ea240fb672ceeac7d34570a7fbfcf78cf6
Botmasher <languagejosh@hotmail.com>
2018-02-24 00:51:46 -1000

    Link nodes and set vector blur

--- nodes_autoset.py
+++ nodes_autoset.py
@@ -6,19 +6,18 @@ import bpy
 ## 
 ## Implement my custom node setup in my Blender anim.blend files
 
+# TODO check if script already ran
+
 def setup_nodes_ui(window=0):
+	"""Set active node editor areas to display the compositor node tree"""
 	for area in bpy.context.window_manager.windows[window].screen.areas:
-		if area.type == "NODE_EDITOR":
-			area.spaces.active.tree_type = "CompositorNodeTree"
-		#bpy.context.space_data.tree_type = "CompositorNodeTree"
-	# TODO set window default when opening a new NODE_EDITOR
+		if area.type == "NODE_EDITOR": area.spaces.active.tree_type = "CompositorNodeTree"
 	return area
 
-def setup_nodes(scene):
-	scene.use_nodes = True
+def setup_nodes():
+	"""Create a render node tree, link nodes and set values"""
+	bpy.context.scene.use_nodes = True
 	
-	#scene.node_tree.type = "COMPOSITING"
-
 	starting_area = bpy.context.area.type
 	bpy.context.area.type = "NODE_EDITOR"
 
@@ -28,22 +27,32 @@ def setup_nodes(scene):
 	for i in range(len(bpy.context.scene.node_tree.nodes)):
 		bpy.context.scene.node_tree.nodes.remove(i)
 
-	# add nodes
-	bpy.ops.node.add_node(type="CompositorNodeRLayers", use_transform=True)
-	bpy.context.scene.node_tree.nodes.active.location = (-250, 0)
-	bpy.ops.node.add_node(type="CompositorNodeVecBlur", use_transform=True)
-	bpy.context.scene.node_tree.nodes.active.location = (0, 0)
-	bpy.ops.node.add_node(type="CompositorNodeComposite", use_transform=True)
-	bpy.context.scene.node_tree.nodes.active.location = (250, 0)
+	node_tree = bpy.context.scene.node_tree
 
-	# TODO link node sockets
+	# add nodes
+	input_render_node = node_tree.nodes.new(type="CompositorNodeRLayers")
+	calc_vector_node = node_tree.nodes.new(type="CompositorNodeVecBlur")
+	output_composite_node = node_tree.nodes.new(type="CompositorNodeComposite")
+
+	# place and link nodes
+	input_render_node.location = (-250, 0)
+	calc_vector_node.location = (0, 0)
+	output_composite_node.location = (250, 0)
+	node_tree.links.new(input_render_node.outputs['Image'], calc_vector_node.inputs['Image'])
+	node_tree.links.new(input_render_node.outputs['Alpha'], output_composite_node.inputs['Alpha'])
+	node_tree.links.new(input_render_node.outputs['Depth'], calc_vector_node.inputs['Z'])
+	node_tree.links.new(calc_vector_node.outputs['Image'], output_composite_node.inputs['Image'])
+
+	# set node values
+	calc_vector_node.samples = 10
+	calc_vector_node.factor = 0.55
+
+	# add speed
+	bpy.context.scene.render.layers.active.use_pass_vector = True
+	node_tree.links.new(input_render_node.outputs['Vector'], calc_vector_node.inputs['Speed'])
 
 	bpy.context.area.type = starting_area
 
-	#node_r_layers = node_group.nodes.new(type="CompositorNodeRLayers")
-	#node_vector_blur = node_group.nodes.new(type="CompositorNodeVectorBlur")
-	#node_composite = node_group.nodes.new(type="CompositorNodeComposite")
+	return node_tree
 
-	return 
-
-setup_nodes(bpy.context.scene)
+setup_nodes()
