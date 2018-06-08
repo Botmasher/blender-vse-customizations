1289fde71dfdc7ea50eaba8c30beecd44c18c193
Botmasher <languagejosh@hotmail.com>
2018-06-08 06:41:17 -1000

    Check if keyframes match an overshoot in Keyframe Overshooter

--- keyframe_overshooter.py
+++ keyframe_overshooter.py
@@ -13,11 +13,25 @@ class KeyframeOvershooter:
 		self.overshoot_strength = 0.2
 		self.rebound_frames = 2
 		self.rebound_bounce = True
+		# TODO add extra rebound bounces getting recursively smaller and switching polarity
 		return
 
-	def analyze_keyframes(self):
+	def analyze_keyframes(self, keyframes, obj, attr):
 		"""Check if the current selection matches an overshoot"""
-		return
+		expected_kf_count = 2 + self.rebound_bounce 	# overshot + rebound + target
+		is_overshoot = False
+		# TODO update all getattr to read val at exact kf
+		base_attr = getattr(obj, attr)
+		if len(keyframes) != expected_kf_count:
+			return is_overshoot
+		for kf in len(keyframes):
+			if kf == 1 and getattr(obj,attr) == base_attr * self.overshoot_strength:
+				return True
+			elif kf > 2:
+				# TODO check that rebounds get smaller each time
+			else:
+				continue
+		return is_overshoot
 
 	def overshoot(self, kf_origin, kf_destination):
 		"""Turn current selected keyframes into an overshoot-rebound-settle animation"""
