d68781a66182f44c24a40859993d971d6c99aa78
Botmasher <languagejosh@hotmail.com>
2018-06-14 11:19:16 -0700

    Interpolate active object's fcurves in Keyframe Overshooter

--- keyframe_overshooter.py
+++ keyframe_overshooter.py
@@ -6,23 +6,23 @@ import bpy
 ## Overshoot i% over n frames before settling into a final animated value.
 ##
 
-def overshoot():
+def overshoot(obj=bpy.context.scene.objects.active, ctx=bpy.context, interpolation_type='BACK'):
 	"""Give current selected keyframes a dynamic interpolation"""
-	obj = bpy.context.scene.objects.active
-	areas = bpy.context.screen.areas
-	if not obj.animation_data or not obj.animation_data.action:
-		print("No keyframes found - select an object")
+	if not obj or not obj.animation_data or not obj.animation_data.action:
+		print("No keyframes found - select an animated object")
 		return None
-	for area in areas:
-		if area.type == 'DOPESHEET_EDITOR':
-			for fcurve in area.spaces[0].action.fcurves:
-				if fcurve.select:
-					print(fcurve)
-					# TODO set curve interpolation to dynamic
-					# area = bpy.context.area.type
-					# bpy.context.area.type = 'GRAPH_EDITOR'
-					# bpy.ops.graph.interpolation_type(type='BACK')
-					# bpy.context.area.type = area
+	area = ctx.area.type
+	ctx.area.type = 'GRAPH_EDITOR'
+	for curve in obj.animation_data.action.fcurves:
+		
+		# TODO check for current keyframe - above will set all curves for active obj
+		
+		if curve.select:
+			for kp in curve.keyframe_points:
+				kp.interpolation = interpolation_type
+		# curve.select and bpy.ops.graph.interpolation_type(type='BACK')
+
+	ctx.area.type = area
 	return
 
 overshoot()
