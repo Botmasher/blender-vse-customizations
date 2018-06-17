d3b15ae09ff046799b683c5c138d53e3b7d6e10b
Botmasher <languagejosh@hotmail.com>
2018-06-17 13:37:51 -0700

    Set untransparent alpha correctly in Untransparent Tex

--- anim/untransparent-alpha-tex.py
+++ anim/untransparent-alpha-tex.py
@@ -7,34 +7,39 @@ import bpy
 ## Configure material+texture with transparent image to have a custom background color
 ##
 
-# Basic steps:
-# - get current material
-# - get current tex
-# - switch current tex alpha to 0.0
-# - switch current material alpha to on (verify)
-# - set current material alpha to 1.0
+def set_untransparent(texture_slot, is_transparent=False):
+	"""Adjust alpha on rendered texture"""
+	texture_slot.texture.use_preview_alpha = is_transparent
+	texture_slot.use_map_alpha = is_transparent
+	return texture_slot
 
-def untransparent_texture(material=bpy.context.active_object.active_material, toggle=False, color=None, set_all=False):
+def untransparent_texture(material=bpy.context.active_object.active_material, toggle=False, color=None, set_all=True):
 	"""Configure a material and its image textures to show a diffuse background behind transparent images"""
+	
+	# verify reference to material and texture (including slot)
 	if not material: return
-	texture = mat.active_texture
+
+	texture = material.active_texture
+
 	if not texture or not texture.type == 'IMAGE': return
 
-	# remove alpha from tex render
-	texture.use_preview_alpha = False
-	texture.use_map_alpha = False
+	texture_slot = material.texture_slots[material.active_texture_index]
 
-	# adjust alpha for opaque mat render
+	# remove alpha from texture render
+	set_untransparent(texture_slot, is_transparent=toggle)
+
+	# adjust opaque material render
 	material.preview_render_type = 'FLAT'
-	material.use_transparency = True
+	material.use_transparency = not toggle
 	material.transparency_method = 'Z_TRANSPARENCY'
-	material.alpha = 1.0
-
-	# TODO correctly add color to mat to appear as tex img bg
+	material.alpha = 1.0 if not toggle else 0.0
 	material.diffuse_color = color
 
-	# TODO set all textures for this material (since material-wide alpha settings are changing)
-	for texture in material.texture_slots:
-		continue
+	# set all textures for this material (since material-wide alpha settings are changing)
+	if set_all:
+		for texture_slot in material.texture_slots:
+			texture_slot and set_untransparent(texture_slot, is_transparent=toggle)
 
 	return (material, texture)
+
+untransparent_texture(color=(1.0, 1.0, 1.0))
