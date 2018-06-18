6beea29f0940bfb8f85c2b4369440ee8e49cfb04
Botmasher <languagejosh@hotmail.com>
2018-06-18 10:45:05 -0700

    Configure blend material-texture gradient

--- anim/tex_blend.py
+++ anim/tex_blend.py
@@ -6,29 +6,60 @@ import bpy
 ##
 ## set colors and options to create a gradient material
 
-# NOTE
-# - create material on active object
-# - create texture (check if slots empty or if one exists)
-# - set to blend
+# extend list methods
+class Grab(list):
+    def first(self):
+        return self[0]
+    def mid(self):
+        return self[int(len(self)*0.5)]
+    def last(self):
+        return self[len(self)-1]
 
+# base name for blends
 blend_name = 'gradient'
 
-def create_blend(obj=bpy.context.scene.objects.active, only_active=False):
+def create_blend(obj=bpy.context.scene.objects.active, colors=Grab([]), only_active=False):
     """Create and assign gradient material-texture to object"""
-    if not obj or not obj.data or not obj.data.materials: return
+
+    if not obj or not obj.data: return
+
     # create and slot in blend material-texture
     material = bpy.data.materials.new(name=blend_name)
+    print(material.name)
     obj.data.materials.append(material)
     texture = bpy.data.textures.new(name=blend_name, type='BLEND')
     material.texture_slots.add()
-    material.texture_slots.create(len(material.texture_slots)-1)
-    material.texture_slots[len(material.texture_slots)-1] = texture
+
+    # TODO exception on material or texture stack overflow
+    texture_slot = None
+    for i in range(len(material.texture_slots)):
+        if not material.texture_slots[i].texture:
+            material.texture_slots.create(i)
+            material.texture_slots[i].texture = texture
+            texture_slot = material.texture_slots[i]
+            break
+
+    # configure blend
+    if len(colors) >= 2 and texture_slot:
+        material.diffuse_color = colors.first()
+        material.diffuse_intensity = 1.0
+        material.specular_intensity = 0.0
+        material.use_transparent_shadows = True
+        texture_slot.use_map_alpha = True
+        texture_slot.texture_coords = 'GENERATED'
+        texture_slot.use_map_color_diffuse = True
+        texture_slot.color = colors.last()
 
     # TODO deactivate all but blend
     if only_active:
         slots = [slot for slot in obj.material_slots if slot.material == material]
         if len(slots) < 1: return
+
     return
+
+# test call
+colors = Grab([(1,1,1), (0,0,0)])
+create_blend(colors=colors)
 
 # TODO create new blended plane
 
