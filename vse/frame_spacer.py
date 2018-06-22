91daa9dce11eacc8b5f55b7400e65d02ded8002b
Botmasher <languagejosh@hotmail.com>
2018-06-22 13:33:49 -0700

    Sort and space strips in frame spacer

--- vse/frame_spacer.py
+++ vse/frame_spacer.py
@@ -8,13 +8,13 @@ import bpy
 ## Lengthen and separate sequences. Ideal for short strips during manual editing.
 ## NOTE script logic is derived from framesplitter.py.
 
-def gap_push_strip(sequence, gap):
+def gap_push_strip(sequence, gap=0):
 	"""Insert a gap after the strip and push all subsequent strips ahead"""
 	bpy.context.scene.frame_current = sequence.frame_start + 1
 	bpy.ops.sequencer.gap_insert(frames=gap)
 	return sequence
 
-def move_strip(sequence, offset):
+def move_strip(sequence, offset=0):
 	"""Move the strip to a new origin along the x axis"""
 	sequence.frame_start = offset
 	return sequence
@@ -24,23 +24,30 @@ def extend_strip(sequence, x_dir):
 	sequence.frame_offset_end -= x_dir
 	return sequence
 
+def order_strips(strips):
+	"""Reorder a list of sequences by final frame from latest to earliest"""
+	strips_by_final_frame = {}
+	for strip in strips:
+		strips_by_final_frame[] = strip
+	# TODO reorder based on sorted keys
+	return strips
+
 def space_strips(strips, extension=0, gap=0):
 	"""Cut a sequencer strip into uniformly stepped and spaced substrips.
-
 	Arguments:
 	strips -- list of sequences in the sequence editor
 	extension -- the left or right hand lengthening in frames applied to each strip
 	gap -- the empty space in frames to leave between each strip
 	"""
-
-	for strip in reverse(strips):
+	strips = order_strips(strips)
+	for strip in strips:
+		bpy.context.scene.frame_current = strip.frame_start
 		strip.select = True
-
-		move_strip(strip, bpy.context.scene.frame_current + (extension + gap))
-		extend_strip(strip, extension)
-
-        strip.select = False
-
+		if strip != strips[0]:
+			move_strip(strip, gap=bpy.context.scene.frame_current + (extension + gap))
+		extend_strip(strip, gap=extension)
+		strip.select = False
 	return strips
 
-space_strips(bpy.context.scene.sequence_editor.sequences)
+strips = [strip for strip in bpy.context.scene.sequence_editor.sequences if strip.select]
+space_strips(strips extension=20, gap=10)
