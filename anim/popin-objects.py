6e84add27773885553413bb1ec58647c421a3962
Botmasher <languagejosh@hotmail.com>
2018-06-27 09:27:01 -0700

    Wrap popin in handler for test calls

--- anim/popin-objects.py
+++ anim/popin-objects.py
@@ -10,12 +10,15 @@ import bpy
 ## 3) rebound to final size
 ##
 
-hide_dir = 2    # one value from (0,1,2) representing x,y,z
-hide_magnitude = -5.0
-scaleup_frames = 6
-rebound_frames = 3
+def keyframe(frame, obj, prop):
+    """Keyframe a property on an object at this frame"""
+    if not obj.animation_data: obj.animation_data_create()
+    # grab fcurve for this prop
+    fcurve = None
+    obj.animation_data.keyframe_insert(fcurve, frame)
+    return obj
 
-def pop_in(obj=bpy.context.scene.objects.active, start_frame=bpy.context.scene.frame_current, hide_vec=(0,0,-1), scale_frames=0, rebound_frames=0):
+def popin(obj=bpy.context.scene.objects.active, start_frame=bpy.context.scene.frame_current, hide_vec=(0,0,-1), scale_frames=0, rebound_frames=0):
     end_loc = obj.location
     end_size = obj.scale
     bpy.context.scene.frame_current = start_frame
@@ -36,6 +39,8 @@ def pop_in(obj=bpy.context.scene.objects.active, start_frame=bpy.context.scene.f
     obj.scale = end_size
     return obj
 
-hide_vec = [0,0,0]
-hide_vec[hide_dir] = hide_magnitude
-pop_in(hide_vec=hide_vec, scale_frames=scaleup_frames, rebound_frames=rebound_frames)
+def popin_handler(hide_vec=[0,0,0], hide_dir=2, hide_magnitude=0):
+    hide_vec[hide_dir] = hide_magnitude
+    return popin(hide_vec=hide_vec, scale_frames=scaleup_frames, rebound_frames=rebound_frames)
+
+popin_handler(hide_magnitude=-5, scaleup_frames=6, rebound_frames=3)
