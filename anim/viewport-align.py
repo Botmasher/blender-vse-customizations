c796bf27be54c42ddb06c4b4e5f96ccea7a7268b
Botmasher <languagejosh@hotmail.com>
2018-07-09 10:46:50 -0700

    Detect vertices outside frustum correctly for viewport align

--- anim/viewport-align.py
+++ anim/viewport-align.py
@@ -62,7 +62,7 @@ def center_in_cam_view(obj=bpy.context.object, cam=bpy.context.scene.camera, dis
 
 # Find object edges vs camera view edges
 
-def get_frustum_loc(point, cam, scene):
+def get_frustum_loc(point, cam=bpy.context.scene.camera, scene=bpy.context.scene):
     """Determine location of a point within camera's rendered frame"""
     if not point or not cam or not scene: return
     # scene to use for frame size
@@ -72,7 +72,7 @@ def get_frustum_loc(point, cam, scene):
     # returns a Vector magnitude 3 with valid cam positions between 0<=uv_loc<=1
     #   - values for index 0,1 greater than 1 are above top-right of frame
     #   - values at index 2 less than 0 are behind camera
-    print(uv_loc)
+    #print(uv_loc)
     return uv_loc
 
 def is_frustum_loc(point, cam=bpy.context.scene.camera, scene=bpy.context.scene):
@@ -81,31 +81,37 @@ def is_frustum_loc(point, cam=bpy.context.scene.camera, scene=bpy.context.scene)
     uv_loc = bpy_extras.object_utils.world_to_camera_view(scene, cam, point)
     return (0.0 <= uv_loc[0] <= 1.0 and 0.0 <= uv_loc[1] <= 1.0 and uv_loc[2] >= 0.0)
 
-def fit_to_frustum(obj, cam=bpy.context.scene.camera):
+def fit_to_frustum(obj, cam=bpy.context.scene.camera, distance=5.0, distort=False):
+    if not hasattr(obj.data, 'vertices'): return    # no mesh to check
     vertex_extremes = [0.0, 0.0, 0.0]
     for vertex in obj.data.vertices:
-        uv = get_frustum_loc(vertex.co)
-        if 0 > uv[0] > 1 and abs(uv[0]) > abs(vertex_extremes[0]):
-            vertex_extremes[0] = uv[0]
-        if 0 > uv[1] > 1 and abs(uv[1]) > abs(vertex_extremes[1]):
-            vertex_extremes[1] = uv[1]
+        uv = get_frustum_loc(obj.matrix_world * vertex.co, cam=cam)
+        print(uv)
+        if uv[0] > 1 or uv[0] < 0:
+            if abs(uv[0]) > abs(vertex_extremes[0]):
+                vertex_extremes[0] = uv[0]
+        if uv[1] > 1 or uv[0] < 0:
+            if abs(uv[1]) > abs(vertex_extremes[1]):
+                vertex_extremes[1] = uv[1]
         if uv[2] < 0 and abs(uv[2]) > abs(vertex_extremes[2]):
             vertex_extremes[2] = uv[2]
     for extreme in vertex_extremes:
-        # TODO use u,v,w extremes to size or move obj
-        # - calculate percent uv outside frustum
-        # - use high u,v diff to size obj down along X, Y
-        # - use high negative w to move obj in front of cam and recurse for u,w
-        if vertex_extremes[2] < 0.0:
-            # move along Z
-            pass
-        elif abs(vertex_extremes[0]) > 1.0:
+        # TODO use highest negative w to move object in front of cam and recurse for u,w
+        # /!\ currently just trusts that object is in front of cam /!\
+        if vertex_extremes[2] < 0.0 :
+           # move along Z
+           # obj.location += (uv[2] - 1.0 - distance)
+           pass
+        if abs(vertex_extremes[0]) > 1.0:
             if abs(vertex_extremes[1]) > 1.0 and abs(vertex_extremes[1] > vertex_extremes[0]):
                 # scale along y
                 pass
             # scale along x
             pass
+    print (vertex_extremes)
     return vertex_extremes
+
+fit_to_frustum(bpy.context.scene.objects.active)
 
 # Work through detecting object in camera again:
 # https://blender.stackexchange.com/questions/45146/how-to-find-all-objects-in-the-cameras-view-with-python/45324
