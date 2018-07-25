0656251e97fb4f2a795c87487fa3f0c738956af4
Botmasher <languagejosh@hotmail.com>
2018-07-25 08:47:30 -0700

    Order strips by start frame before shuffling

--- vse/shuffle_strips.py
+++ vse/shuffle_strips.py
@@ -3,20 +3,23 @@ from random import shuffle
 
 ## Shuffle Selected Strips
 
-def shuffle_strips(strips, frame):
-    lengths = []
-    start_frame = None
+def move_strips_away(strips):
+    original_start_frame
     for s in strips:
         if not start_frame or s.frame_start < start_frame:
             start_frame = s.frame_start
-        lengths.append(s.length)
         s.frame_start -= 9999999999     # move all strips out of the way
+    return original_start_frame
+
+def shuffle_strips(strips, frame):
+    #start_frame = move_strips_away(strips)
+    start_frame = strips[0].frame_start
     shuffle(strips)
     for s in strips:
         s.frame_start = start_frame
         start_frame += s.length
     return strips
 
-strips = [s for s in bpy.context.scene.sequence_editor.sequences if s.select]
+strips = sorted((s for s in bpy.context.scene.sequence_editor.sequences if s.select), key=lambda x: x.frame_start)
 start_frame = min([s.frame_start for s in strips])
-shuffle(strips)
+shuffle_strips(strips)
