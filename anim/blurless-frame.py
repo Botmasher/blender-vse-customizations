51c9213836a7e2b0530d3b6310f84925317cd21a
Botmasher <languagejosh@hotmail.com>
2018-08-10 07:19:21 -0700

    Define props and UI class, panel for blurless vec blur frames

--- anim/blurless-frame.py
+++ anim/blurless-frame.py
@@ -1,11 +1,16 @@
 import bpy
+import bpy.props
 
-## Blurless Frame
+## Blurless Frames
 ##
 ## Toggle and keyframe a blurless stint for vector blur nodes
 ## example use: jump camera in a single frame without blur during movement
 
-# TODO: config bpy.props to pass blurless_frames and adjust factor
+def define_blurless_props(types=bpy.types):
+    """Create properties for adjusting blurless vec blur length and strength"""
+    types.Scene.blurless_frames = bpy.props.IntProperty(name="Blurless length", description="Frame count of blurless vec blur stints", default=1)
+    types.Scene.blurless_factor = bpy.props.FloatProperty(name="Blurless factor", description="How low to set vec blur during blurless frames", default=0.0)
+    return
 
 def is_node_tree(obj):
     """Check if the object is a node tree"""
@@ -37,26 +42,76 @@ def key_vecblur(node, factor, framejump=0):
     node.keyframe_insert('factor')
     return node
 
-def set_blurless_node(node, frames):
+def set_blurless_node(node, frames, factor):
     """Keyframe turning vector blurring off then back on later"""
     if not is_vecblur_node(node):
         return False
     blur = node.factor
-    blurless_rel_keyframes = ((blur, -1), (0.0, 1), (0.0, frames), (blur, 1))
+    blurless_rel_keyframes = ((blur, -1), (factor, 1), (factor, frames), (blur, 1))
     for kf in blurless_rel_keyframes:
         key_vecblur(node, kf[0], framejump=kbf[1])
     return True
 
-def handle_blurless_stint(tree=bpy.context.scene.node_tree, blurless_frames=1):
+def handle_blurless_stint(tree=bpy.context.scene.node_tree, blurless_frames=1, blurless_factor=0.0):
     """Turn off blurring on all vector blur nodes during frames count"""
     if not is_node_tree(tree) or not blurless_frames:
         return
     nodes = get_vecblur_nodes(tree)
     for node in nodes:
         node.type == 'VECBLUR' and print(node)
-        set_blurless_node(node, blurless_frames)
+        set_blurless_node(node, blurless_frames, blurless_factor)
     return nodes
 
 # test run
 blurless_time = 10  # int prop
 handle_blurless_stint(blurless_frames=10)
+
+class BlurlessPanel(bpy.types.Panel):
+	bl_label = "Blurless"
+	bl_idname = 'scene.blurless_panel'
+	bl_space_type = "VIEW_3D"
+	bl_region_type = "TOOLS"
+	bl_category = "Blurless"
+
+	def draw(self, ctx):
+
+        define_blurless_props()
+
+		layout = self.layout
+		col = layout.column(align=True)
+
+		col.label("Config blurless:")
+		col.row().prop(scene, "blurless_frames")
+        col.row().prop(scene, "blurless_factor")
+
+		col.label("Run blurless:")
+		row = col.row(align=True)
+		row.operator('scene.blurless_op', text="blurless stint")
+
+		return
+
+class BlurlessOperator(bpy.types.Operator):
+	bl_label = "Blurless Frames"
+	bl_idname = 'scene.blurless_op'
+	bl_description = "Keyframe blurless vec blur frames"
+	bl_options = {'REGISTER', 'UNDO'}
+
+	@classmethod
+	def poll(c, ctx):
+		return ctx.mode == "OBJECT"
+
+    def execute(self, context):
+        frames = bpy.context.scene.blurless_frames
+        factor = bpy.context.scene.blurless_factor
+        handle_blurless_stint(blurless_frames=frames, blurless_factor=factor)
+        return {'FINISHED'}
+
+def register():
+	bpy.utils.register_class(BlurlessPanel)
+	bpy.utils.register_module(__name__)
+
+def unregister():
+	bpy.utils.unregister_class(BlurlessPanel)
+	bpy.utils.unregister_module(__name__)
+
+__name__ == "__main__" and register()
