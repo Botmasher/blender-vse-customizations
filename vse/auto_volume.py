727c93a8ebf7038e42f8cadc7cc2e58f9d2f38a2
Botmasher <languagejosh@hotmail.com>
2018-08-13 07:00:57 -0700

    Abstract mass volume functions

--- vse/auto_volume.py
+++ vse/auto_volume.py
@@ -1,10 +1,11 @@
+import bpy
+import re
+
 ####
 # MASS AUDIO VOLUME SET
-# script by Botmasher (Josh Rudder)
+# script by GitHub user Botmasher
 # automatically adjust the volume of audio strips in your VSE sequencer!
 ####
-
-import bpy
 
 # scene name for sequencer
 sequencer_scene_name = 'Scene'
@@ -12,18 +13,31 @@ sequencer_scene_name = 'Scene'
 target_strips_name = 'audio-'
 
 # new target volume to set
-new_vol = 1.92
+updated_volume = 1.92
 
-# search through all sequencer strips
-for i in bpy.data.scenes[sequencer_scene_name].sequence_editor.sequences:
-    bpy.context.scene.sequence_editor.active_strip=None
-    # find strips matching your name pattern
-    if target_strips_name in i.name:
-        bpy.context.scene.sequence_editor.active_strip=i
-        # adjust the volume of the active strip
-        try:
-            bpy.data.scenes[sequencer_scene_name].sequence_editor.sequences_all[i.name].volume=new_vol              
-        except:
-            pass
-    else:
-        pass
+def is_audio_sequence(obj):
+    if obj and hasattr(obj, 'volume'):
+        return True
+    return False
+
+def set_volume(strip, new_volume):
+    if is_audio_sequence(strip):
+        strip.volume = new_volume
+        return True
+    return False
+
+def filter_strips(strips, match_name, strip_type='AUDIO'):
+    r_name = re.compile(match_name)
+    matches = []
+    for strip in strips:
+        match = re.match(r_name, strip.name)
+        match and strip.type == strip_type and matches.append(strip)
+    return matches
+
+def handle_mass_vol(strips=bpy.context.scene.sequence_editor.sequences, name='', volume=1.0):
+    target_strips = filter_strips(strips, match_name=name)
+    for strip in target_strips:
+        set_volume(strip, volume)
+    return strips
+
+handle_mass_vol(volume=updated_volume)
