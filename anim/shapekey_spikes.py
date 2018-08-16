1dd0c166e71b3d9f86f911dda7275cb56b408591
Botmasher <languagejosh@hotmail.com>
2018-08-16 09:00:42 -0700

    Store basic shape key and playhead for shapekey spikes

--- anim/shapekey_spikes.py
+++ anim/shapekey_spikes.py
@@ -19,6 +19,21 @@ import bpy
 # - move ahead frames
 # - keyframe active to original value
 
+def current_frame():
+    scene = bpy.context.scene
+    def set_ahead(frames_ahead):
+        scene.frame_current += frames_ahead
+    return set_ahead
+
+def shapekey_spike(target_val=1.0):
+    sk = bpy.context.scene.objects.active.active_shape_key
+    sk_i = bpy.context.scene.objects.active.active_shape_key_index
+    sks = bpy.context.scene.objects.active.data.shape_keys
+    # get object from sk
+    obj = sks.user
+    playhead = current_frame()
+    return
+
 # TODO add props for custom config
 # - number of frames on spike sides
 # - asymmetrical boolean if spike sides have different frames
@@ -26,6 +41,8 @@ import bpy
 # - number of frames at top of spike
 
 # TODO add interface
+# - discover how to add UI to shape keys tab of object
+# Props UI:
 # - slider for 0.0-1.0 target value
 # - number of frames on spike sides
 # - checkbox for asymmetrical sides
