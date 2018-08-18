bc02b557064870ab3624d82b4102469b76e97586
Botmasher <languagejosh@hotmail.com>
2018-08-18 07:21:22 -0700

    Repair shapekey checks and add basic ui for shapekey spike

--- anim/shapekey_spikes.py
+++ anim/shapekey_spikes.py
@@ -19,7 +19,7 @@ import bpy
 # - move ahead frames
 # - keyframe active to original value
 
-def current_frame():
+def current_frame_setter():
     scene = bpy.context.scene
     def set_ahead(frames_ahead):
         scene.frame_current += frames_ahead
@@ -27,10 +27,11 @@ def current_frame():
     return set_ahead
 
 def is_shape_key(object):
-    return object and object.bl_rna and object.bl_rna.rna_type == 'Shape Key'
+    return object and object.bl_rna and object.bl_rna.name == 'Shape Key'
 
 def set_shape_keyframe(shape_key, key_value):
     if not is_shape_key(shape_key):
+        print("Not a shape key: {0}".format(shape_key))
         return
     shape_key.value = key_value
     shape_key.keyframe_insert('value')
@@ -41,7 +42,7 @@ def get_active_shape_key(object):
 
 def spike_shape_key(object=bpy.context.object, target_val=1.0, spike_frames=1, left_frames=1, asymmetrical=False, right_frames=1):
 
-    if not object or not hasattr(object, 'active_shape_key') not target_val or not left_frames:
+    if not object or not hasattr(object, 'active_shape_key') or not target_val or not left_frames:
         return
 
     shape_key = get_active_shape_key(object)
@@ -57,7 +58,7 @@ def spike_shape_key(object=bpy.context.object, target_val=1.0, spike_frames=1, l
     #obj = shape_keys.user
 
     # store timeline movement method
-    move_playhead = current_frame()
+    move_playhead = current_frame_setter()
 
     # create spike keyframes
     set_shape_keyframe(shape_key, initial_val)
@@ -70,13 +71,26 @@ def spike_shape_key(object=bpy.context.object, target_val=1.0, spike_frames=1, l
 
     return shape_key
 
-spike_shape_key()
+# test run
+shapes = {
+    'blink': {
+        'left': 3,
+        'right': 0,
+        'mid': 2,
+        'asymmetrical': False
+    }
+}
+spike_shape_key(left_frames=shapes['blink']['left'], spike_frames=shapes['blink']['mid'])
 
-# TODO add props for custom config
-# - number of frames on spike sides
-# - asymmetrical boolean if spike sides have different frames
-# - number of frames on downward side if asymmetrical
-# - number of frames at top of spike
+def add_props():
+    # TODO add props for custom config
+    # - number of frames on spike sides
+    # - asymmetrical boolean if spike sides have different frames
+    # - number of frames on downward side if asymmetrical
+    # - number of frames at top of spike
+    return
+
+# UI
 
 # TODO add interface
 # - discover how to add UI to shape keys tab of object
@@ -86,3 +100,40 @@ spike_shape_key()
 # - checkbox for asymmetrical sides
 #   - if checked show number of frames on return side
 # - number of spike frames
+
+class ShapekeySpikePanel (bpy.types.Panel):
+    bl_label = "Shapekey Spike"
+    bl_context = "objectmode"
+    bl_idname = "object.shapekey_spike_panel"
+    bl_space_type = "VIEW_3D"
+    bl_region_type = "TOOLS"
+    def draw (self, context):
+        obj = bpy.context.object
+        rows = [self.layout.row() for i in range(5)]
+        # TODO add props
+        rows[0].prop(obj, "shapekey_spike_frames_left")
+        rows[1].prop(obj, "shapekey_spike_frames_mid")
+        rows[2].prop(obj, "shapekey_spike_frames_right")
+        rows[3].prop(obj, "shapeky_spike_asymmetrical")
+        props = rows[4].operator("object.shapekey_spike", text="Spike Shape Key")
+        return
+
+class ShapekeySpikeOperator (bpy.types.Operator):
+    bl_label = "Shapekey Spike Operator"
+    bl_idname = "object.shapekey_spike"
+    bl_description = "Take shape key to a target value then return to original value"
+
+    def execute (self, context):
+        obj = bpy.context.object
+        hasattr(obj, 'active_shape_key') and spike_shape_key()
+        return {'FINISHED'}
+
+def register():
+    bpy.utils.register_class(PopinOperator)
+    bpy.utils.register_class(PopinPanel)
+
+def unregister():
+    bpy.utils.unregister_class(PopinPanel)
+    bpy.utils.unregister_class(PopinOperator)
+
+__name__ == '__main__' and register()
