0c03758ef9f9f940f8742f1894f572255ed25e3a
Botmasher <languagejosh@hotmail.com>
2018-08-19 08:36:38 -0700

    Set up props for shapekey spikes

--- anim/shapekey_spikes.py
+++ anim/shapekey_spikes.py
@@ -1,4 +1,5 @@
 import bpy
+from bpy.props import *
 
 ## Shape Key Spikes
 ##
@@ -40,7 +41,7 @@ def set_shape_keyframe(shape_key, key_value):
 def get_active_shape_key(object):
     return object.active_shape_key
 
-def spike_shape_key(object=bpy.context.object, target_val=1.0, spike_frames=1, left_frames=1, asymmetrical=False, right_frames=1):
+def spike_shape_key(object=bpy.context.object, target_val=1.0, spike_frames=1, left_frames=1, asymmetric=False, right_frames=1):
 
     if not object or not hasattr(object, 'active_shape_key') or not target_val or not left_frames:
         return
@@ -52,11 +53,6 @@ def spike_shape_key(object=bpy.context.object, target_val=1.0, spike_frames=1, l
 
     initial_val = shape_key.value
 
-    # retrieve other potentially relevant objects
-    #shape_key_i = bpy.context.scene.objects.active.active_shape_key_index
-    #shape_keys = bpy.context.scene.objects.active.data.shape_keys
-    #obj = shape_keys.user
-
     # store timeline movement method
     move_playhead = current_frame_setter()
 
@@ -66,56 +62,40 @@ def spike_shape_key(object=bpy.context.object, target_val=1.0, spike_frames=1, l
     set_shape_keyframe(shape_key, target_val)
     move_playhead(spike_frames)
     set_shape_keyframe(shape_key, target_val)
-    current_frame = move_playhead(right_frames) if asymmetrical else move_playhead(left_frames)
+    current_frame = move_playhead(right_frames) if asymmetric else move_playhead(left_frames)
     set_shape_keyframe(shape_key, initial_val)
 
     return shape_key
 
-# test run
-shapes = {
-    'blink': {
-        'left': 3,
-        'right': 0,
-        'mid': 2,
-        'asymmetrical': False
-    }
-}
-spike_shape_key(left_frames=shapes['blink']['left'], spike_frames=shapes['blink']['mid'])
-
-def add_props():
-    # TODO add props for custom config
-    # - number of frames on spike sides
-    # - asymmetrical boolean if spike sides have different frames
-    # - number of frames on downward side if asymmetrical
-    # - number of frames at top of spike
+def add_shapekey_spike_props():
+    O = bpy.types.Object
+    # number of frames on spike sides
+    O.shapekey_spike_frames_left = IntProperty(name="Side Length", description="Frames to the left and right of spike target value (just left if asymmetric)", default=1)
+    # number of frames at top of spike
+    O.shapekey_spike_frames_mid = IntProperty(name="Mid Length", description="Frames to hold spike target value", default=1)
+    O.shapekey_spike_value = FloatProperty(name="Spike target value", description="", default=1.0)
+    # number of frames on downward side if asymmetrical
+    O.shapekey_spike_frames_right = IntProperty(name="Right length", description="Frames to the right of spike target value (only if asymmetric)", default=1)
+    # spike sides have different frames
+    O.shapekey_spike_asymmetric = BoolProperty(name="Asymmetric", description="Option to use different frame counts to the left and right of spike target value", default=False)
     return
-
-# UI
-
-# TODO add interface
-# - discover how to add UI to shape keys tab of object
-# Props UI:
-# - slider for 0.0-1.0 target value
-# - number of frames on spike sides
-# - checkbox for asymmetrical sides
-#   - if checked show number of frames on return side
-# - number of spike frames
 
 class ShapekeySpikePanel (bpy.types.Panel):
     bl_label = "Shapekey Spike"
     bl_context = "objectmode"
     bl_idname = "object.shapekey_spike_panel"
-    bl_space_type = "VIEW_3D"
-    bl_region_type = "TOOLS"
+    bl_category = "Shape Keys"
+    bl_space_type = "OUTLINER"
+    bl_region_type = "WINDOW"
     def draw (self, context):
         obj = bpy.context.object
-        rows = [self.layout.row() for i in range(5)]
-        # TODO add props
+        rows = [self.layout.row() for i in range(6)]
         rows[0].prop(obj, "shapekey_spike_frames_left")
         rows[1].prop(obj, "shapekey_spike_frames_mid")
-        rows[2].prop(obj, "shapekey_spike_frames_right")
-        rows[3].prop(obj, "shapeky_spike_asymmetrical")
-        props = rows[4].operator("object.shapekey_spike", text="Spike Shape Key")
+        rows[2].prop(obj, "shapekey_spike_value")
+        rows[3].prop(obj, "shapekey_spike_frames_right")
+        rows[4].prop(obj, "shapeky_spike_asymmetric")
+        props = rows[5].operator("object.shapekey_spike", text="Spike Shape Key")
         return
 
 class ShapekeySpikeOperator (bpy.types.Operator):
@@ -125,15 +105,17 @@ class ShapekeySpikeOperator (bpy.types.Operator):
 
     def execute (self, context):
         obj = bpy.context.object
-        hasattr(obj, 'active_shape_key') and spike_shape_key()
+        if hasattr(obj, 'active_shape_key'):
+            spike_shape_key(left_frames=obj.shapekey_spike_frames_left, spike_frames=obj.shapekey_spike_frames_mid, right_frames=obj.shapekey_spike_frames_right, target_val=obj.shapekey_spike_value, asymmetric=obj.asymmetric)
         return {'FINISHED'}
 
 def register():
-    bpy.utils.register_class(PopinOperator)
-    bpy.utils.register_class(PopinPanel)
+    add_shapekey_spike_props()
+    bpy.utils.register_class(ShapekeySpikeOperator)
+    bpy.utils.register_class(ShapekeySpikePanel)
 
 def unregister():
-    bpy.utils.unregister_class(PopinPanel)
-    bpy.utils.unregister_class(PopinOperator)
+    bpy.utils.unregister_class(ShapekeySpikePanel)
+    bpy.utils.unregister_class(ShapekeySpikeOperator)
 
 __name__ == '__main__' and register()
