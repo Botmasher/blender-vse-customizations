35da3df74e068054aafcd50770a23f22d6a6736f
Botmasher <languagejosh@hotmail.com>
2018-09-09 10:09:04 -0700

    Pack new selected strips into repacked meta

--- vse/meta-unpack-repack.py
+++ vse/meta-unpack-repack.py
@@ -3,14 +3,19 @@ import bpy
 def is_strip(obj):
     return hasattr(obj, 'bl_rna') and 'Sequence' in obj.rna_type.name
 
-def has_meta_rna(obj):
-    return hasattr(obj, 'bl_rna') and obj.bl_rna.name == 'Meta Sequence'
+def is_meta(obj):
+    return hasattr(obj, 'bl_rna') and obj.bl_rna.name == 'Meta Sequence' and hasattr(obj, 'type') and obj.type == 'META'
 
-def has_meta_type(obj):
-    return hasattr(obj, 'type') and obj.type == 'META'
+def get_selected_strips(strip_type=None):
+    strips = []
+    for s in bpy.context.scene.sequence_editor.sequences:
+        if is_strip(s):
+            if not strip_type or s.type == strip_type:
+                strips.append(s)
+    return strips
 
 def unpack_meta(strip):
-    if not has_meta_type(strip):
+    if not is_meta(strip):
         return [strip]
     bpy.ops.sequencer.meta_separate()
     strips = [s for s in bpy.context.scene.sequence_editor.sequences if s.select]
@@ -28,12 +33,23 @@ def pack_meta(strips):
 def run_op(strips, op):
     return op(strips)
 
-def unpack_repack(meta):
+def unpack_add_repack_meta(meta, add_selected=False):
+
+    added_strips = []
+    if add_selected:
+        added_strips = get_selected_strips()
+
     strips = unpack_meta(meta)
-    op = bpy.ops.sequencer.gap_insert()     # example operation
-    run_op(op)
+
+    # example operation
+    #op = bpy.ops.sequencer.gap_insert()
+    #run_op(op)
+
+    # add new strips to meta
+    strips = [*strips, *added_strips]
     pack_meta(strips)
-    return
+
+    return bpy.context.scene.sequence_editor.active_strip
 
 m = bpy.context.scene.sequence_editor.active_strip
-unpack_meta(m)
+unpack_add_repack_meta(m)
