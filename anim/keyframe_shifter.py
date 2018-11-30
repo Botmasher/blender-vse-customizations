ec8a89045b7b0c740e872550483fa853598e2845
Botmasher <languagejosh@hotmail.com>
2018-11-30 08:28:56 -0800

    Create class around keyframe shifter

--- anim/keyframe_shifter.py
+++ anim/keyframe_shifter.py
@@ -7,35 +7,53 @@ import bpy
 
 scene = bpy.context.scene
 
-# NOTE trial run - shift every keyframe in one obj
-def shift_keyframes(obj=None, frameshift=0, is_relative=True):
-    """Shift an object's keyframes forwards or backwards in timeline"""
-    if not (obj and obj.animation_data and frameshift):
-        return
+# NOTE basic shift on every keyframe in one obj
 
-    # set relative to current kf frame xor absolute to frame number
-    #   - ? absol very limited use since set all to same frame
+class KeyframeShifter:
+    def __init__(self):
+        self.shifted_objects = []
+        self.current_object = None
+        self.modified_keyframes = {}
 
-    try:
-        obj.animation_data.action.fcurves
-    except:
-        raise Exception("Failed to shift keyframes using keyframe_shifter - object contains no animation fcurves: {0}".format(obj))
+    def is_anim_object(self):
+        if not (self.current_object and self.current_object.animation_data):
+            return False
+        if not self.current_object.animation_data.action.fcurves:
+            raise Exception("Failed to shift keyframes using keyframe_shifter - object contains no animation fcurves: {0}".format(self.current_object))
+            return False
+        return True
 
-    obj_fcurves = [*obj.animation_data.action.fcurves]
-    modified_kfs = []
+    def set_obj(self, obj):
+        if not is_anim_object(obj):
+            return
+        self.current_object = obj
+        return self.current_object
 
-    # set all kfs within all anim fcurves
-    for fcurve in obj_fcurves:
-        for kf in fcurve.keyframe_points:
-            if is_relative:
-                kf.co.x += frameshift
-            else:
-                kf.co.x = frameshift
-            modified_kfs.append(kf)
+    def _shift_keyframes(self, frameshift=0):
+        """Internal method to shift an object's keyframes forwards or backwards in timeline"""
+        obj_fcurves = [*obj.animation_data.action.fcurves]
+        modified_kfs = []
 
-    print(modified_kfs)
-    return modified_kfs
+        # set all kfs within all anim fcurves
+        for fcurve in obj_fcurves:
+            for kf in fcurve.keyframe_points:
+                modified_kfs.append(kf)
+
+        return modified_kfs
+
+    def shift(self, obj, frameshift=0):
+        """Set the current object and run the keyframe shift"""
+        current_obj = self.set_obj(obj)
+        kfs = self.shift_keyframes(frameshift=frameshift)
+        if not (current_obj and kfs and frameshift):
+            return
+        # TODO revisit storage after allowing separate attr adjustments
+        self.modified_keyframes[self.current_object] = kfs
+        return kfs
 
 # TODO filter by keyframe attr (like transform, shape, ...)
 
-shift_keyframes(obj=scene.objects.active, frameshift=10)
+kf_shifter = KeyframeShifter()
+kf_shifter.shift(scene.objects.active, frameshift=10)
+
+# TODO ui
