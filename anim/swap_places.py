ba542c5b5eb562d82e19c45f5204080a451494c2
Botmasher <languagejosh@hotmail.com>
2019-09-22 01:22:18 -1000

    Swap object places for pairs of objects (straight-line) and begin animating mid frames

--- anim/swap_places.py
+++ anim/swap_places.py
@@ -8,40 +8,56 @@ clockwise = False   # or counterclock
 horizontal = True   # or vert
 frame_length = 20   # length of the swap in frames
 
-def get_selected_objects():
+def get_selected():
     """List all objects currently selected in scene"""
-    selected_objs = [
-        obj for obj in bpy.context.scene.objects
-        if obj.select
-    ]
-    return selected_objs
+    return bpy.context.view_layer.objects.selected
 
 # TODO: order objects by position in cam
+def pair_elements(elements, cut_straggler=False):
+    # pair all pairable inpit
+    object_pairs = [
+        [elements[i], elements[i+1]]
+        for i in range(0, len(elements), 2)
+        if i + 1 < len(elements)
+    ]
+    # add lone nonpair if uncut
+    if not cut_straggler and not len(elements) % 2:
+        object_pairs.append([elements[-1]])
+    
+    return object_pairs
+
+def move_and_keyframe(obj, loc):
+    obj.keyframe_insert('location')
+    obj.location = loc
+    obj.keyframe_insert('location')
+
 def swap_objects(objects_list):
     # pair off objects
-    obj_pairs = []
-    for obj in objects_list:
-        if not obj_pairs or len(obj_pairs[-1]) > 1:
-            obj_pairs.append([obj])
-        else:
-            obj_pairs[-1].append(obj)
-    # leave off stranded nonpair
-    if len(obj_pairs[-1]) != 2:
-        obj_pairs = obj_pairs[:-1]
+    obj_pairs = pair_elements(objects_list, True)
 
     # swap objects in each pair
     # TODO: optimize positioning, add horiz vs vert positioning
     for obj_pair in obj_pairs:
-        obj_a, obj_b = obj_pair
-        loc_a = obj_a.location
-        loc_b = obj_b.location
-        obj_a.keyframe_insert('location')
-        obj_b.keyframe_insert('location')
-        bpy.context.scene.frame_current += frame_length
-        obj_a.location = loc_b
-        obj_a.keyframe_insert('location')
-        obj_b.location = loc_a
-        obj_b.keyframe_insert('location')
+        if len(obj_pair) != 2:
+            raise ValueError(f"Unpaired, unswappable objects in {obj_pair}")
+        (obj_a, obj_b) = obj_pair
+        loc_a = obj_a.location[:]
+        loc_b = obj_b.location[:]
+        move_and_keyframe(obj_a, loc_a)
+        move_and_keyframe(obj_b, loc_b)
+        
+        bpy.context.scene.frame_current += int(frame_length * 0.5)
+        mid_loc_a = loc_b + ((loc_a - loc_b) * 0.5)
+        mid_loc_b = loc_a + ((loc_a - loc_b) * 0.5)
+        # TODO: move up/down out of the way midway in view
+        mid_loc_a.z += 2.5 if clockwise else -2.5
+        mid_loc_b.z += -2.5 if clockwise else 2.5
+        move_and_keyframe(obj_a, mid_loc_a)
+        move_and_keyframe(obj_b, mid_loc_b)
+        
+        bpy.context.scene.frame_current += int(frame_length * 0.5)
+        move_and_keyframe(obj_a, loc_b)
+        move_and_keyframe(obj_b, loc_a)
 
-objs = get_selected_objects()
+objs = get_selected()
 swap_objects(objs)
